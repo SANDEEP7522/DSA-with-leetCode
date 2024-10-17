8af8369b5381e88eb92435a6c1270a1d9980efe7
SANDEEP7522 <159306588+SANDEEP7522@users.noreply.github.com>
2024-10-17 13:40:10 +0530

    844. Backspace String Compare

---
+++ stack/stack-844.js
@@ -0,0 +1,34 @@
+// 844. Backspace String Compare
+
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var backspaceCompare = function(s, t) {
+    const st1 = [];
+    const st2 = [];
+    for(let i = 0; i < s.length; i++){
+     if(st1.length > 0 && s[i] == "#"){
+         st1.pop();
+     }else{
+         st1.push(s[i]);
+     }
+    }
+     for(let i = 0; i < t.length; i++){
+     if(st2.length > 0 && t[i] == "#"){
+         st2.pop();
+     }else{
+         st2.push(t[i]);
+     }
+    }
+     while(st1.length >0 && st2.length > 0){
+         if( st1[st1.length-1] != st2[st2.length-1]  ){
+             return false;
+         }
+         st1.pop();
+         st2.pop();
+     }
+     return (st1.length == 0) && (st2.length == 0);
+ };
